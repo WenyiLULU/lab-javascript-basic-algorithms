0249e0dc0d9a0fb98ef6c0eb24a832d34490d3bb
WenyiLULU <luwenyi1991@gmail.com>
2022-05-02 19:47:01 +0200

    Bonus done

--- js/index.js
+++ js/index.js
@@ -52,4 +52,46 @@ if (hacker1 < hacker2) {
     console.log("What?! You both have the same name?");
 };
 
+/* Bonus 1:
+Go to lorem ipsum generator and:
 
+Generate 3 paragraphs. Store the text in a variable type of string.
+Make your program count the number of words in the string.
+Make your program count the number of times the Latin word et appears.*/
+
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mollis, risus sed feugiat porta, velit metus commodo eros, tempor viverra orci mauris id felis. Phasellus fringilla mollis pellentesque. Aliquam volutpat lobortis eros id suscipit. Morbi luctus et ante id pretium. Etiam condimentum eros vel dui placerat, quis vehicula diam efficitur. Sed tincidunt ex ex, id commodo quam semper ut. Curabitur suscipit ligula mauris. Ut et urna luctus, placerat erat at, aliquet tortor. Ut metus turpis, lacinia vel neque vitae, mattis vehicula orci. Maecenas vitae felis a ipsum molestie cursus. Phasellus elementum mauris elit, quis suscipit lacus congue in. Ut in leo non risus pellentesque placerat eget non eros. Vestibulum sodales et ligula ut eleifend. Donec leo tortor, laoreet vitae tristique ut, pulvinar vitae eros. Morbi sed elit ac orci porta lacinia.
+Pellentesque justo dolor, mattis sit amet est lobortis, pretium consequat magna. Aliquam bibendum consectetur nisl, in gravida mauris lobortis at. Quisque suscipit odio vitae leo rutrum efficitur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas vel nisi sit amet arcu vestibulum laoreet. Donec arcu enim, luctus et ultrices at, porttitor sed velit. Duis finibus, mi ac lobortis congue, urna enim varius eros, in finibus lacus nunc vitae metus.
+Etiam nec leo eget velit molestie sodales. Donec sed massa vestibulum, faucibus ex bibendum, pulvinar felis. Aliquam erat volutpat. Vestibulum gravida tortor non sapien vehicula, quis condimentum libero ornare. Maecenas eget varius dui. Maecenas ultrices accumsan odio sit amet efficitur. Pellentesque et tincidunt tortor, id tincidunt felis. Integer facilisis sem nisl, vel lobortis quam aliquet eget. Mauris laoreet quam vitae nulla euismod sagittis. Curabitur dapibus ex nibh, nec ornare nisl faucibus id. In vitae sodales enim, non bibendum quam. `
+
+let words = text.replace(/[\n]/, ' ').split(' ');
+console.log(words.length);
+let count = 0;
+for (let i = 0; i < words.length; i+=1) {
+    if(words[i] === 'et' || words[i] === 'et,' || words[i] === 'et.'){
+        count +=1;
+    }
+};
+console.log (count);
+
+/*Bonus 2:
+Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
+
+"A man, a plan, a canal, Panama!"
+"Amor, Roma"
+"race car"
+"stack cats"
+"step on no pets"
+"taco cat"
+"put it up"
+"Was it a car or a cat I saw?" and "No 'x' in Nixon". */
+let phraseToCheck = "A man, a plan, a canal, Panama!"
+let letters = phraseToCheck.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+let reverseLetters = "";
+for (let i = letters.length - 1; i >= 0; i-=1) {
+    reverseLetters += letters[i];
+};
+if (reverseLetters === letters){
+    console.log(`"${phraseToCheck}" is a Palindrome.`);
+} else {
+    console.log(`"${phraseToCheck}" is not a Palindrome.`);
+};
